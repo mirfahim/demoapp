581fdba21498c6675b2d6657047b53b4eecb713e
mir fahim <mirfahim390@gmail.com>
2020-09-14 15:18:09 +0600

    new

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// my own packages
+import 'package:demoapp/pages/home.dart';
+
+main() {
+  runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Home(),
+  ));
+}
+
